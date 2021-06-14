4ebc495f736cdac47ea9a0b4f9b2fd014c8827c9
Liheng Cao <lihengcao@nyu.edu>
2021-06-14 17:39:49 -0400

    started walk (simulation)

--- slider.py
+++ slider.py
@@ -1,7 +1,8 @@
 import time
+import numpy as np
 
 import dynamic_graph_manager_cpp_bindings
-from robot_properties_solo.solo8wrapper import Solo8Config 
+from robot_properties_solo.solo8wrapper import Solo8Config
 
 # Create the dgm communication to the control process.
 head = dynamic_graph_manager_cpp_bindings.DGMHead(Solo8Config.dgm_yaml_path)
@@ -11,9 +12,9 @@ D = 0.05
 dt = 0.001
 next_time = time.time() + dt
 
-# new
-# constant
-K = 6
+K = 5
+
+angle_adjust = np.array([1,-2,1,-2,-1,2,-1,2]) * np.pi
 
 
 # In your control loop:
@@ -21,31 +22,20 @@ while (True):
     if time.time() >= next_time:
         next_time += dt
 
-        ###
-        # Get the latest measurements from the shared memory.
         head.read()
 
-        ###
-        # A very simple example D controller.
-
-        # Read the joint velocities.
-        # joint_velocities = head.get_sensor('joint_velocities')
-
-        # new
-        slider_pos = head.get_sensor('slider_positions')[0]
+        slider_pos = head.get_sensor('slider_positions')
         joint_pos = head.get_sensor('joint_positions')
         joint_velocities = head.get_sensor('joint_velocities')
 
-        # Set the joint torques.
-        # tau = -D * joint_velocities
 
-        # new
-        tau = K*(slider_pos - joint_pos) - D * joint_velocities
+        # target = np.array()
+        target = slider_pos[0]/2 * angle_adjust
+
+
+        tau = K*(target - joint_pos) - D * joint_velocities
 
         head.set_control('ctrl_joint_torques', tau)
-
-        ###
-        # Write the results into shared memory again.
         head.write()
 
     time.sleep(0.0001)
