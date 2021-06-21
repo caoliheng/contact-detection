ee72dcddbdc96343715b9b835b025b4096c1f4bd
Liheng <lihengcao@nyu.edu>
2021-06-21 17:09:37 -0400

    tested contact sensor, changed setup script

--- setup scripts/jupyter_terminal.bash
+++ setup scripts/jupyter_terminal.bash
@@ -6,5 +6,4 @@ source /opt/openrobots/setup.bash
 source install/setup.bash
 
 cd src/contact-detection
-jupyter notebook --allow-root --no-browser
-
+~/.local/bin/jupyter-lab --allow-root --no-browser
