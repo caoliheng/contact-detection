2b7fffb1391702bc20df3e95e46a53ead5128786
Liheng Cao <lihengcao@nyu.edu>
2021-07-28 17:29:01 -0400

    /

--- setup scripts/c.bash
+++ setup scripts/c.bash
@@ -5,5 +5,5 @@
 source /opt/ros/dashing/setup.bash
 source /opt/openrobots/setup.bash
 cd ~/Desktop/dev/workspace
-colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
+colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release -DPYBIND11_TEST=OFF
 source ~/Desktop/dev/workspace/install/setup.bash
